5037a5fedb7a8be3393cf896b94c3d956de7696c
an-nikol <voynskaa@gmail.com>
2024-06-22 13:29:15 +0300

    Added solutions

---
+++ 01_django_model_basics_exercise/05_product/main_app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+import datetime
+# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.PositiveIntegerField()
+
+class Blog(models.Model):
+    post = models.TextField()
+    author = models.CharField(max_length=35)
+
+
+class WeatherForecast(models.Model):
+    date = models.DateField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    precipitation = models.FloatField()
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    cook_time = models.PositiveIntegerField()
+    created_at = models.DateTimeField(default=datetime.datetime.today())
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(default=datetime.datetime.today())
